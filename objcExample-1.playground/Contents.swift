a421257a5550fcedc85ff354c0d4215b15d4ad5e
김광준 <mskang@xingxingmobility.com>
2022-07-28 05:51:19 +0900

    Swift Data Structure Example code

--- objcExample-1.playground/Contents.swift
+++ objcExample-1.playground/Contents.swift
@@ -3,3 +3,17 @@ import Foundation
 var postalCode: NSString = NSString(format: "%d-%d", 32259,1234)
 var len = postalCode.length
 print("postal code length: \(len)\n")
+
+@objc(ObjCMovieList)
+class MovieList: NSObject {
+    private var tracks = ["The Godfather", "Homealone", "The Dark Knight"]
+    
+    subscript(index: Int) -> String {
+        get {
+            return self.tracks[index]
+        }
+        set {
+            self.tracks[index] = newValue
+        }
+    }
+}
