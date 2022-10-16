d37da84a9c27b0014e28d45dc1e02f726020c7e1
김광준 <mskang@xingxingmobility.com>
2022-10-16 18:57:48 +0900

    패스트캠퍼스 github app init

---
+++ FC_Github/FC_Github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FC_Github
+//
+//  Created by Minseong Kang on 2022/10/16.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ FC_Github/Pods/Target Support Files/Pods-FC_Github-FC_GithubUITests/Pods-FC_Github-FC_GithubUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FC_Github_FC_GithubUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FC_Github_FC_GithubUITests
+@end
